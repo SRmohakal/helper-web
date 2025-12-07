c6406d310a38616dee48a27ecba08beb8b37ec46
SRmohakal <royshourov71@gmail.com>
2025-12-07 07:20:57 +0600

    fixing webhook problem

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ app.use(clerkMiddleware())
 
 
 app.get('/', (req,res)=>{res.send("Api Working")})
-app.post('/clerk', express.json(), clerkWebhooks)
+
+app.use('/clerk', express.raw({ type: 'application/json' }));
+app.post('/clerk', clerkWebhooks);
+
 app.use('/api/educator', express.json(), educatorRouter);
 app.use('/api/course', express.json(), courseRouter);
 app.use('/api/user', express.json(), userRouter);
